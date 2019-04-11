9f54c00a5009b9e14ead543536a17aeb40d53c78
rjsworking <rjsworking@gmail.com>
2019-04-11 12:19:18 +0100

    Update AliasFile.php

--- src/AliasFile.php
+++ src/AliasFile.php
@@ -12,6 +12,7 @@ class AliasFile
     public static function global(): string
     {
         $home = isset($_SERVER['HOME'])?$_SERVER['HOME']:$_SERVER['HOMEDRIVE'].$_SERVER['HOMEPATH'];
+        
         return $home . '/.laravel_aliases';
     }
 }
